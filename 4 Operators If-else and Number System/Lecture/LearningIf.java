e1e79d26038c14742b5e0219f369285f65c61402
Shaam26 <shameembanu561@gmail.com>
2025-09-27 19:47:58 +0530

    Added files

---
+++ 4 Operators If-else and Number System/Lecture/LearningIf.java
@@ -0,0 +1,29 @@
+public class LearningIf {
+    public static void main(String[] args) {
+//        boolean isMale = false;
+//        String name = "Bob";
+//
+//        System.out.println("before if");
+//        if(isMale){
+//            System.out.println("Mr. "+name);
+//        }else{
+//            System.out.println("Ms. "+name);
+//        }
+//        System.out.println("after if");
+
+
+
+        boolean isSeniorCitizen = true;
+        boolean isAnAdult = false;
+
+        if(isSeniorCitizen){
+            System.out.println("Hello senior citizen");
+        } else if(isAnAdult){
+            System.out.println("Hello Adult");
+            } else{
+                System.out.println("Hello child");
+            }
+        }
+
+    }
+}
