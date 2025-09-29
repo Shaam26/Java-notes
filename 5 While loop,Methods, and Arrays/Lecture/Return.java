cabad5939fd80c9299f307fdcb439ab34922fae7
Shaam26 <shameembanu561@gmail.com>
2025-09-29 08:05:06 +0530

    Added files

---
+++ 5 While loop,Methods, and Arrays/Lecture/Return.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Return {
+    public static void main(String[] args) {
+
+       greet();
+        int first = readNumber() ;
+        int second = readNumber() ;
+
+        int sum = first+second;
+        System.out.println("Sum of number is: "+sum);
+    }
+
+    public static int readNumber(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter the number: ");
+        int number = input.nextInt();
+        return number;
+
+    }
+
+    public static void greet(){
+        System.out.println("Welcome to Calculator\n");
+    }
+}
