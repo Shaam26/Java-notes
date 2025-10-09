3ec91da8f46f8a9ff4273223b3a033d4b5a4e06b
Shaam26 <shameembanu561@gmail.com>
2025-10-09 22:45:59 +0530

    Added files

---
+++ Challenges/ArrayChallenges/ReverseArray.java
@@ -0,0 +1,19 @@
+class ReverseArray {
+    public static void main(String[] args) {
+        System.out.println("welcome to Array Reverse\n");
+        int []  numArr = ArrayUtility.inputArray();
+        reverse(numArr);
+        ArrayUtility.displayArray(numArr);
+    }
+    public static void reverse(int[] arr){
+        int i = 0;
+        while(i < arr.length / 2){
+            int swap = arr[i];
+            arr[i] = arr[(arr.length - 1) - i];
+            arr[(arr.length - 1) - i] = swap;
+            i++;
+
+
+        }
+    }
+}
