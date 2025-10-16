a083952ae8d4f8a03f35031af08bb143497b30a9
Shaam26 <shameembanu561@gmail.com>
2025-10-16 06:07:11 +0530

    Added files

--- 6 classes and objects/Lecture/Car.java
+++ 6 classes and objects/Lecture/Car.java
@@ -1,24 +1,63 @@
-class Car {
+public class Car {
+
+    static int noOfCarsSold;
    int noOfWheels;
    String color;
    float maxSpeed;
    float currentFuelInLiters;
    int noOfSeats;
 
-   public void drive(){
-       if (currentFuelInLiters==0){
-           System.out.println("Car is out of fuel");
-       }else if(currentFuelInLiters < 5){
-           System.out.println("Car is in reserved mode, please refuel");
-           currentFuelInLiters--;
-       }else {
-           System.out.println("Car is driving");
-           currentFuelInLiters--;
-       }
+
+   static{
+       noOfCarsSold = 0;
+       System.out.println("I am in Static Block");
+   }
+    {
+        noOfCarsSold++;
+        System.out.println("I am in Init Block ");
+    }
+
+   Car(String color) {
+      noOfWheels =4;
+      this.color = color;
+      maxSpeed = 150;
+      currentFuelInLiters=2;
+      noOfSeats = 5;
 
    }
-   public void addFuel(float fuel){
-       currentFuelInLiters += fuel;
+    Car() {
+        noOfWheels =4;
+        this.color = "black";
+        maxSpeed = 150;
+        currentFuelInLiters=2;
+        noOfSeats = 5;
+
+    }
+
+   public Car start(){
+       int pop = 5;
+       if (currentFuelInLiters==0){
+
+           System.out.println("Car is out of fuel,it cannot start");
+       }else if(currentFuelInLiters < 5){
+           System.out.println("Car is in reserved mode, please refuel");
+       }else {
+           System.out.println("Car is started...bruhhh...");
+       }
+       System.out.println(pop);
+       return this;
+
+   }
+
+
+   public void drive(){
+           currentFuelInLiters--;
+           System.out.println("Car is driving");
+       }
+
+
+   public void addFuel(float currentFuelInLiters){
+       this.currentFuelInLiters += currentFuelInLiters;
 
    }
    public float getCurrentFuelLevel(){
--- 6 classes and objects/Lecture/Driver.java
+++ 6 classes and objects/Lecture/Driver.java
@@ -1,10 +1,33 @@
 public class Driver {
-    public static void main(String[] args) {
-        Car myCar = new Car();
-        myCar.addFuel(6);
-        myCar.drive();
-        myCar.drive();
-        myCar.drive();
-        System.out.println(myCar.getCurrentFuelLevel());
+
+
+    static int minAgeForDriving = 18;
+        String name;
+        int  age;
+        String dateOfLicense;
+
+        public boolean isAllowedToDrive(){
+            return this.age >= minAgeForDriving;
+        }
+
+    public static void main(String[] args){
+//        Car myCar = new Car();
+//        myCar.addFuel(6);
+//        myCar.drive();
+//        myCar.drive();
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+//
+        Car swift = new Car("red");
+        Car thar = new Car();
+        thar  = null;
+//        swift.addFuel(6);
+        swift.start().drive();
+        System.out.println(swift.color);
+
+//        Driver myDriver = new Driver();
+//        myDriver.dateOfLicense = "1/Jan/2024";
+//        System.out.println(minAgeForDriving);
+
     }
 }
