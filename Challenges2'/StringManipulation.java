9eb092132f23aaefbf4823fcb874cdc108e89146
Shaam26 <shameembanu561@gmail.com>
2025-11-06 08:35:57 +0530

    Added files

---
+++ Challenges2'/StringManipulation.java
@@ -0,0 +1,8 @@
+ class StringManipulation {
+     public static void main(String[] args) {
+         String FirstName ="Shameem";
+         String LastName = "Banu";
+         String FullName = FirstName.concat(" ").concat(LastName);
+         System.out.println(FullName.toUpperCase());
+     }
+}
---
+++ Challenges2'/StudentToString.java
@@ -0,0 +1,27 @@
+ class StudentToString {
+    String name;
+    int age;
+    String rollNumber;
+    String house;
+
+
+     public StudentToString(String name, int age, String rollNumber, String house) {
+         this.name = name;
+         this.age = age;
+         this.rollNumber = rollNumber;
+         this.house = house;
+     }
+     @Override
+     public String toString(){
+         return "Student Details:{name:"+name
+         +",age:"+age
+         +",rollNumber:"+rollNumber
+         +",house:"+house + "}";
+     }
+
+
+     public static void main(String[] args) {
+         StudentToString stu = new StudentToString("Shameem",26,"786","Mangalore");
+         System.out.println(stu);
+     }
+ }
