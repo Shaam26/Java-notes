56e4d8ff9a76079ff1477c1eefaca018431a38e5
Shaam26 <shameembanu561@gmail.com>
2025-11-09 21:04:09 +0530

    Added files

---
+++ 9 Abstraction And Polymorphism/Lecture/in/Coding/abstraction/Car.java
@@ -0,0 +1,14 @@
+package in.Coding.abstraction;
+
+public class Car extends Vehicle{
+    private int noOfDoors;
+    public Car(){
+        super(4);
+    }
+
+    @Override
+    public void makeStartSound(){
+        System.out.println("Vrooom....");
+
+    }
+}
