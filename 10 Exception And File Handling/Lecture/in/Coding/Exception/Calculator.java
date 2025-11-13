550b44ffc8a85be76ca0fd0e7f4699a3715dd800
Shaam26 <shameembanu561@gmail.com>
2025-11-13 09:37:42 +0530

    Added files

---
+++ 10 Exception And File Handling/Lecture/in/Coding/Exception/Calculator.java
@@ -0,0 +1,27 @@
+package in.Coding.Exception;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to division calculator\n");
+        System.out.println("please enter your two number: ");
+        int first = input.nextInt();
+        int second = input.nextInt();
+
+
+        try {
+            int [] a = new int[5];
+            System.out.printf("Result is %d",a[6]);
+           a[6] = first / second;
+           System.out.printf("Result is %d", a[6]);
+        }catch(ArithmeticException exception) {
+            System.out.printf("%s, enter valid values",
+                    exception.getMessage());
+        }catch(Throwable th){
+            System.out.println("General Exception.");
+        }
+    }
+
+}
