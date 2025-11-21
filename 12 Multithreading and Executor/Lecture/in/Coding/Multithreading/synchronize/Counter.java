b8645ed7fd26cd53e2b5348136b0dea8b9d1d70a
Shaam26 <shameembanu561@gmail.com>
2025-11-21 21:52:42 +0530

    Added files

---
+++ 12 Multithreading and Executor/Lecture/in/Coding/Multithreading/synchronize/Counter.java
@@ -0,0 +1,12 @@
+package in.Coding.Multithreading.synchronize;
+
+public class Counter {
+    private int count = 0;
+    public synchronized void increment(){
+        count++;
+
+    }
+    public int getCount(){
+        return count;
+    }
+}
