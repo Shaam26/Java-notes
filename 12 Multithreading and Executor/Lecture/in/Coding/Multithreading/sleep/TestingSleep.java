18c50502d8e9a6e17a674f6af5cf95a99159082a
Shaam26 <shameembanu561@gmail.com>
2025-11-21 22:21:58 +0530

    Added files

---
+++ 12 Multithreading and Executor/Lecture/in/Coding/Multithreading/sleep/TestingSleep.java
@@ -0,0 +1,10 @@
+package in.Coding.Multithreading.sleep;
+
+public class TestingSleep {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("before sleeping");
+        Thread.sleep(5000);
+        System.out.println("Woke up");
+
+    }
+}
---
+++ Challenges2'/in/Kgcoding/Challenge100/ThreadState.java
@@ -0,0 +1,16 @@
+package in.Kgcoding.Challenge100;
+
+public class ThreadState extends Thread{
+
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(4000);
+            System.out.printf("\nFrom inside run %s ",
+                    getState());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
