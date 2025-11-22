ae6ec3b6a1d66ff00238b72a2d1cc411af2ac839
Shaam26 <shameembanu561@gmail.com>
2025-11-22 21:16:58 +0530

    Added files

---
+++ Challenges2'/in/Kgcoding/Challenge105/TestingFactorial.java
@@ -0,0 +1,29 @@
+package in.Kgcoding.Challenge105;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+
+public class TestingFactorial {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        try (ExecutorService service = Executors.newFixedThreadPool(3)) {
+            List<Future<Integer>> list = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                FactorialCalculator task = new FactorialCalculator(i);
+                list.add(service.submit(task));
+            }
+
+            for (Future<Integer> future : list) {
+                System.out.printf("\n Result is: %d", future.get());
+            }
+
+            service.shutdown();
+
+            if (!service.awaitTermination(10, TimeUnit.SECONDS)) {
+                System.out.println("\nBOHOT HUA BAS....");
+                service.shutdown();
+            }
+        }
+    }
+}
